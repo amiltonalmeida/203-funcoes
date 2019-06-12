63367062ed3adf9c0c08df52e6c1b3ec0e0cde8c
Amilton <amiltonfelipedc@gmail.com>
2019-06-12 16:41:39 -0300

    Trabalhar com funções no javascript

---
+++ script.js
@@ -0,0 +1,110 @@
+// let idade = 105;
+
+// idade = idade + 1;
+// idade += 1;
+
+
+// console.log(idade)
+
+// let nome = "Lucas";
+// let cargo = 'programador';
+// let signo = 'aries';
+
+// alert ("O nome do usuário é " + nome);
+
+// alert (`O nome do usuário é ${nome}, o cargo dele é ${cargo} e o signo dele é ${signo}`);
+
+// construir uma escada
+// material da escada pode ser * # $
+// perguntar quandos degraus elx quer
+
+// +=
+// for in ou for of
+
+// SEGUNDA ETAPA
+// PERGUNTAR QUAL É O MATERIAL QUE O USUÁRIO QUER
+
+
+// let materialEscada = '#';
+// let qntd = prompt ("Quantos dregaus vocÊ vai querer linda: ");
+
+// for(let i=0; i<qntd; i++){
+//     console.log(materialEscada);
+//     materialEscada += "#";
+// }
+
+// let materialEscada = prompt ("Que material voce quer: * # $?");
+// let degrau = materialEscada;
+// let qntd = Number (prompt ("Quantos dregaus vocÊ vai querer linda: "));
+// for(let i=0; i<qntd; i++){
+//     console.log(materialEscada);
+//     materialEscada += degrau;
+// }
+
+
+// let lista = ['batata', 'pastel','tomate','alface'];
+
+// let objeto={
+//     casa:[];
+//     carro:[];
+// }
+
+// let filmes = [
+//     {titulo: 'Harry Potter', idade: 10},
+//     {titulo: 'It, a coisa', idade: 16},
+//     {titulo: 'O albergue', idade: 18}
+// ]
+
+// let idade = Number(prompt('Qual sua idade: '));
+
+// let idade = Number(prompt("Qual sua idade: "));
+// if(filme)
+// console.log(filmes[0].idade);
+// console.log(filmes[1].idade);
+// console.log(filmes[2].idade);
+
+// let valor1 = Number(prompt("Digite o primeiro valor: "));
+// let valor2 = Number(prompt("Digite o segundo valor: "));
+
+// function somar (valor1,valor2){
+//     console.log(valor1+valor2);
+// }
+
+// somar(valor1,valor2);
+
+// function frase (a,b,c){
+//     console.log(a + b + c);
+// }
+
+// frase("oie ","bem ","vinda");
+
+// let N1 = Number(prompt("Digite a primeira nota: "));
+// let N2 = Number(prompt("Digite a segunda nota: "));
+// let N3 = Number(prompt("Digite a terceira nota: "));
+// let N4 = Number(prompt("Digite a quarta nota: "));
+
+
+// function somar (N1,N2,N3,N4){
+//     let media = (N1+N2+N3+N4)/4;
+//     console.log(`Sua média foi ${media}`);
+// }
+
+// somar(N1,N2,N3,N4);
+
+
+// let N1 = Number(prompt("Digite seu primeiro número: "));
+// let N2 = Number(prompt("Digite seu segundo número: "));
+// let operacao = ("Qual operação você deseja realizar? ")
+
+
+// function somar (N1,N2,N3,N4){
+//     let media = (N1+N2+N3+N4)/4;
+//     console.log(`Sua média foi ${media}`);
+// }
+
+// somar(N1,N2,N3,N4);
+
+let fazerConta = function(a,b){
+    console.log(a,b);
+}
+
